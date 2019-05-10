bb96fe84c131cd4de9e74caef6c61f6690426009
Piotr Sarna <piotr@sarna.net.pl>
2019-05-10 22:32:25 +0200

    Add qr code generation

--- Plugins/Pay/PayPlugin.py
+++ Plugins/Pay/PayPlugin.py
@@ -1,3 +1,7 @@
+import io
+import qrcode
+from PIL import Image
+
 from Core.PluginBase import PluginBase
 from Command import Command
 
@@ -10,18 +14,43 @@ class PayPlugin(PluginBase):
         command = Command.parse(message=message.markup)
 
         for contact, order_cost in command.order_costs.iteritems():
-            message = self._prepare_message(command=command, order_cost=order_cost)
+            final_order_cost = self._calculate_final_order_cost(command=command, order_cost=order_cost)
+            message = self._prepare_message(command=command, final_order_cost=final_order_cost)
+            qr_code = self._prepare_qrcode(command=command, final_order_cost=final_order_cost)
 
             self._skype.contacts[contact].chat.sendMsg(message)
+
+            if qr_code:
+                self._skype.contacts[contact].chat.sendFile(qr_code, "qr_code.png", image=True)
 
     def help_message(self):
         # TODO
         return "TODO: Fill help message"
 
     @staticmethod
-    def _prepare_message(command, order_cost):
-        divided_delivery_cost = command.delivery_cost / len(command.order_costs)
-        final_order_cost = order_cost + divided_delivery_cost
-        final_order_cost = float(final_order_cost) / 100
+    def _prepare_message(command, final_order_cost):
+        return "Blik: {}\nNumer konta: {}\nKwota: {}".format(command.blik, command.account_number, float(final_order_cost)/100)
 
-        return "Blik: {}\nNumer konta: {}\nKwota: {}".format(command.blik, command.account_number, final_order_cost)
+    @staticmethod
+    def _prepare_qrcode(command, final_order_cost):
+        if command.account_number is None:
+            return None
+
+        qr_code_text = "|PL|{}|{}|{}|{}|||".format(
+            "".join(command.account_number.split()),
+            final_order_cost,
+            command.account_owner_name.upper() if command.account_owner_name else "",
+            command.transfer_title if command.transfer_title else "",
+        )
+
+        qr_code = qrcode.make(qr_code_text)
+        img_byte_arr = io.BytesIO()
+        qr_code.save(img_byte_arr, format='PNG')
+        img_byte_arr.seek(0)
+
+        return img_byte_arr
+
+    @staticmethod
+    def _calculate_final_order_cost(command, order_cost):
+        divided_delivery_cost = command.delivery_cost / len(command.order_costs)
+        return order_cost + divided_delivery_cost
