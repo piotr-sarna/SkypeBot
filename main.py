614c8969a2922fb02233c963b462f6d1819e078b
Piotr Sarna <piotr@sarna.net.pl>
2019-05-11 13:26:46 +0200

    Add setting presence ONLINE

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     plugins = PluginsLoader().load()
     skype = SkypeListener(username=username, password=password, plugins=plugins)
 
+    skype.setPresence()
     skype.loop()
