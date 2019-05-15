68d02d7720da918c19b7183cb679799d2c9f252d
Piotr Sarna <piotr@sarna.net.pl>
2019-05-15 21:18:45 +0200

    Add rough version of command parsing for Pizza plugin and handling it

---
+++ Plugins/Pizza/Command.py
@@ -0,0 +1,78 @@
+
+class Command:
+    def __init__(self):
+        self.help = False
+        self.number_of_pieces = None
+        self.start = False
+        self.stop = False
+        self.cost = False
+
+    def _set_help(self, value):
+        self.help = True
+
+    def _set_number_of_pieces(self, value):
+        if value:
+            self.number_of_pieces = int(value)
+
+    def _set_start(self, value):
+        self.start = True
+
+    def _set_stop(self, value):
+        self.stop = True
+
+    def _set_cost(self, value):
+        self.cost = True
+
+    def _known_commands(self):
+        return {
+            "#help": self._set_help,
+            "#pizza": self._set_number_of_pieces,
+            "#start": self._set_start,
+            "#stop": self._set_stop,
+            "#cost": self._set_cost,
+        }
+
+    def _prepare_message(self, message):
+        idx = message.rfind("<e_m")
+
+        if idx != -1:
+            message = message[:idx]
+
+        message = message.replace('\t', ' ').replace('\n', ' ').replace('\r', ' ').strip()
+
+        for command in self._known_commands().keys():
+            message = message.replace(command, "\n" + command)
+
+        return message
+
+    def _parse_known_commands(self, message):
+        for line in message.splitlines():
+            line = line.strip()
+
+            if not line.startswith("#"):
+                continue
+
+            for command, resolver in self._known_commands().items():
+                if line.startswith(command):
+                    value = line[len(command):].strip()
+                    resolver(value=value)
+                    break
+
+    @staticmethod
+    def _parse_order_cost(line):
+        split_line = line.split(" ", 1)
+        user_id = split_line[0].strip()[1:]
+        amount = split_line[1].strip().replace(',', '.')
+        amount = int(float(amount) * 100)
+
+        return user_id, amount
+
+    @classmethod
+    def parse(cls, message):
+        result = Command()
+
+        message = result._prepare_message(message=message)
+
+        result._parse_known_commands(message=message)
+
+        return result
--- Plugins/Pizza/PizzaPlugin.py
+++ Plugins/Pizza/PizzaPlugin.py
@@ -1,9 +1,16 @@
-from skpy import SkypeNewMessageEvent
+from skpy import SkypeNewMessageEvent, SkypeEditMessageEvent
 
 from Core.PluginBase import PluginBase
+from .Command import Command
 
 
 class PizzaPlugin(PluginBase):
+    def __init__(self, skype):
+        super(PizzaPlugin, self).__init__(skype=skype)
+        self._orders = []
+        self._started_by = None
+        self._is_stopped = False
+
     def friendly_name(self):
         return 'Pizza plugin'
 
@@ -14,13 +21,32 @@ class PizzaPlugin(PluginBase):
         return ['pizza']
 
     def handle(self, event):
-        return
+        if not isinstance(event, SkypeNewMessageEvent) and not isinstance(event, SkypeEditMessageEvent):
+            return
+
+        message = event.msg
+        command = Command.parse(message=message.markup)
+
+        if self._process_if_help_command(message=message, command=command):
+            return
+
+        self._ensure_exactly_one_command_is_given(command=command)
+
+        if command.start:
+            self._handle_start(message=message, command=command)
+        elif command.stop:
+            self._handle_stop(message=message, command=command)
+        elif command.cost:
+            self._handle_cost(message=message, command=command)
+        elif command.number_of_pieces:
+            self._handle_number_of_pieces(message=message, command=command)
 
     def help_message(self):
         return """{friendly_name} v{version}
 
 Keywords: {keywords}
 Commands:
+    #help
     #start
     #stop
     #cost
@@ -29,3 +55,61 @@ Commands:
            version=self.version(),
            keywords=','.join(['#' + keyword for keyword in self.keywords()])
            )
+
+    def _process_if_help_command(self, message, command):
+        if command.help:
+            self._skype.contacts[message.userId].chat.sendMsg(self.help_message())
+
+        return command.help
+
+    @staticmethod
+    def _ensure_exactly_one_command_is_given(command):
+        commands_number = 0
+        commands_number += 1 if command.start else 0
+        commands_number += 1 if command.stop else 0
+        commands_number += 1 if command.cost else 0
+        commands_number += 1 if command.number_of_pieces else 0
+
+        if commands_number != 1:
+            raise Exception("You have to specify exactly one command at once")
+
+    def _handle_start(self, message, command):
+        if self._started_by:
+            raise Exception("Exactly one #pizza can be started at the same time")
+
+        self._orders = []
+        self._started_by = message.userId
+        self._is_stopped = False
+
+        self._skype.contacts[message.userId].chat.sendMsg("OK")
+
+    def _handle_stop(self, message, command):
+        if not self._started_by:
+            raise Exception("No #pizza is currently started")
+
+        if self._started_by != message.userId:
+            raise Exception("Only user which started #pizza can stop it")
+
+        if self._is_stopped:
+            raise Exception("#pizza is already stopped")
+
+        self._is_stopped = True
+
+        # TODO: send summary to _started_by
+        # TODO: inform users that does not fit into order
+
+        self._skype.contacts[message.userId].chat.sendMsg("OK")
+
+    def _handle_cost(self, message, command):
+        pass
+
+    def _handle_number_of_pieces(self, message, command):
+        if not self._started_by or self._is_stopped:
+            raise Exception("No #pizza is currently started")
+
+        existing_idx = [i for i, v in enumerate(self._orders) if v[0] == message.userId]
+
+        if existing_idx:
+            self._orders.pop(existing_idx[0])
+
+        self._orders.append((message.userId, command.number_of_pieces))
