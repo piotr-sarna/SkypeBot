8ba553f917e31a2d0f50305da3c28d2e27f209b4
Piotr Sarna <piotr@sarna.net.pl>
2020-02-18 22:48:40 +0100

    Add possibility to use 'me' and 'rich' modifiers of Skype client

--- Core/SkypeClient.py
+++ Core/SkypeClient.py
@@ -38,14 +38,14 @@ class SkypeClient(SkypeEventLoop):
                 logger.exception("Non-exception raised")
                 raise
 
-    def send_direct_message(self, user_id, message: str):
-        self.contacts[user_id].chat.sendMsg(content=message)
+    def send_direct_message(self, user_id, message: str, me=False, rich=False):
+        self.contacts[user_id].chat.sendMsg(content=message, me=me, rich=rich)
 
-    def send_direct_response(self, message: str):
-        self.__current_event.msg.user.chat.sendMsg(content=message)
+    def send_direct_response(self, message: str, me=False, rich=False):
+        self.__current_event.msg.user.chat.sendMsg(content=message, me=me, rich=rich)
 
-    def send_group_response(self, message: str):
-        self.__current_event.msg.chat.sendMsg(content=message)
+    def send_group_response(self, message: str, me=False, rich=False):
+        self.__current_event.msg.chat.sendMsg(content=message, me=me, rich=rich)
 
     def __process_message_event(self, event):
         keyword = self.__get_keyword(event=event)
